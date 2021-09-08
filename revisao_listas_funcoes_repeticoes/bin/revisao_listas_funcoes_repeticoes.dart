a859df1398aeb81bb3a76777b8589a8fd60e3737
Alygabi <alycelimagabi@gmail.com>
2021-09-07 22:01:36 -0300

    atividade2

---
+++ revisao_listas_funcoes_repeticoes/bin/revisao_listas_funcoes_repeticoes.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+void main(List<String> arguments) {
+  var mensagem = devolverOlaNome('Alyce');
+  print(mensagem);
+  }
+
+
+void imprimir(){
+  print('Olá pessoas!');
+}
+
+void imprimirNome(String nome) {
+  print(' Ola $nome!');
+}
+
+String devolverOla(){
+  return 'Ola pessoas!';
+}
+
+String devolverOlaNome(String nome) {
+  return 'Ola $nome!';
+}
+//Listas e repetições; 
